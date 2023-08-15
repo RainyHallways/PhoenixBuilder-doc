5057203abc024a01c0b6adc004cf592fafbdd6ee
RainyHallways <78735995+RainyHallways@users.noreply.github.com>
2023-08-15 17:05:12 +0800

    Update chksenswd.vue

--- docs/.vitepress/theme/components/chksenswd.vue
+++ docs/.vitepress/theme/components/chksenswd.vue
@@ -1,8 +1,19 @@
-<script setup>
-import { vue } from "vue";
+<template>
+  <div>
+  <input v-model="userInput" type="text" placeholder="输入文字">
+  <button @click="checkSensitive" :disabled="isLoading">{{ isLoading ? '等待中' : '判断' }}</button>
+  <div v-if="isLoading">正在进行敏感词判断，请稍候...</div>
+  <div v-if="result">
+    敏感词: {{ result[0] }}
+    匹配结果: {{ result[1] }}
+  </div>
+  </div>
+</template>
+
+<script>
 import nreg from "./new_reg.json";
 
-new vue({
+export default {
   data() {
   return {
     userInput: "",
@@ -13,46 +24,38 @@ new vue({
   methods: {
   async checkSensitive() {
     this.isLoading = true;
-    this.result = await checkSensitiveWord(this.userInput);
+    this.result = await this.checkSensitiveWord(this.userInput);
     this.isLoading = false;
   },
-  },
-  template: `
-  <div>
-    <input v-model="userInput" type="text" placeholder="输入文字">
-    <button @click="checkSensitive" :disabled="isLoading">{{ isLoading ? '等待中' : '判断' }}</button>
-    <div v-if="isLoading">正在进行敏感词判断，请稍候...</div>
-    <div v-if="result">
-    敏感词: {{ result[0] }}
-    匹配结果: {{ result[1] }}
-    </div>
-  </div>
-  `,
-}).$mount("#app");
-
-function checkSensitiveWord(word) {
-  return new Promise((resolve) => {
-  setTimeout(() => {
-    for (let val in nreg.regex) {
-    for (let n in nreg.regex[val]) {
-      let cur = new RegExp(
-      nreg.regex[val][n]
-        .replace(/\(\?(i|\#\d)\)/g, "")
-        .replace(/^.*?content=/g, "")
-        .replace(/[\u4e00-\u9fa5]/g, (m) =>
-        String.fromCharCode(m.charCodeAt(0) - 1)
-        ),
-      "ig"
-      );
-      let sm = word.match(cur);
-      if (sm) {
-      resolve([`${val}.${n}`, sm]);
-      return;
+  checkSensitiveWord(word) {
+    return new Promise((resolve) => {
+    setTimeout(() => {
+      for (let val in nreg.regex) {
+      for (let n in nreg.regex[val]) {
+        let cur = new RegExp(
+        nreg.regex[val][n]
+          .replace(/\(\?(i|\#\d)\)/g, "")
+          .replace(/^.*?content=/g, "")
+          .replace(/[\u4e00-\u9fa5]/g, (m) =>
+          String.fromCharCode(m.charCodeAt(0) - 1)
+          ),
+        "ig"
+        );
+        let sm = word.match(cur);
+        if (sm) {
+        resolve([`${val}.${n}`, sm]);
+        return;
+        }
       }
-    }
-    }
-    resolve(null);
-  }, 2000);
-  });
-}
+      }
+      resolve(null);
+    }, 2000);
+    });
+  },
+  },
+};
 </script>
+
+<style scoped>
+/* Add your styles here */
+</style>
